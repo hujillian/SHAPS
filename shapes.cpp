27535ed347116105f065d6edc79c2ebf1afabcae
shortCV <cversace18@gmail.com>
2021-12-11 16:59:42 -0500

    circle class
    update

--- shapes.cpp
+++ shapes.cpp
@@ -11,3 +11,31 @@ void shapes::update()
 void shapes::draw()
 {
 }
+shapes::shapes(int startX, int startY) {
+
+	m_position = ofVec2f(startX, startY);
+	//m_direction = ofVec2f(ofRandom(-2.0, 2.0), ofRandom(-2.0, 2.0)); // direction that the particles move in is random
+		//down, each shape has specific direction
+	m_force = ofVec2f(0, 0); // starting force on the particles is 0
+
+}
+shapes::~shapes()
+{
+	//deconstructor
+}
+
+void circle::draw()
+{
+	colorOne = (174, 218, 251);
+	ofSetColor(colorOne);
+	ofDrawCircle(xPos,yPos,0,5);
+
+}
+
+void rectangle::draw()
+{
+}
+
+void triangle::draw()
+{
+}
--- shapes.h
+++ shapes.h
@@ -2,12 +2,39 @@
 #include "ofMain.h"
 
 class shapes {
+public:
 	float xPos;
 	float yPos;
 	float speedY;
 	float speedX;
 
+
 	void setup();
 	void update();
+	void draw();
+
+	ofVec2f m_force;
+	ofVec2f m_position;
+	ofVec2f m_direction;
+
+	shapes(int startX, int startY);
+	~shapes();
+};
+
+class circle : public shapes {
+public:
+	ofColor colorOne;
+	void draw();
+};
+
+class rectangle : public shapes {
+public:
+	ofColor colorTwo;
+	void draw();
+};
+
+class triangle : public shapes {
+public:
+	ofColor colorThree;
 	void draw();
 };
